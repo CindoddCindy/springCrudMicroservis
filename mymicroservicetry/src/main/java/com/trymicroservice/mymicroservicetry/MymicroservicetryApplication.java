c86db1fb701acdad6dc6ea2ed4bf96ed37710fb0
CindoddCindy <cindodcindy@gmail.com>
2021-06-11 14:38:18 +0700

    baru tambah satu kelas

---
+++ mymicroservicetry/src/main/java/com/trymicroservice/mymicroservicetry/MymicroservicetryApplication.java
@@ -0,0 +1,13 @@
+package com.trymicroservice.mymicroservicetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymicroservicetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymicroservicetryApplication.class, args);
+	}
+
+}
---
+++ mymicroservicetry/src/main/java/com/trymicroservice/mymicroservicetry/rest/entities/User.java
@@ -0,0 +1,34 @@
+package com.trymicroservice.mymicroservicetry.rest.entities;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="username", nullable = false)
+    private String username;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="email", nullable = false)
+    private String email;
+
+
+}
