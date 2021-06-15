28fb4b485d44f3fb2c0e8e3912f68bf10aab2c2f
CindoddCindy <cindodcindy@gmail.com>
2021-06-15 11:11:47 +0700

    hapus validation class

--- mymicroservicetry/src/main/java/com/trymicroservice/mymicroservicetry/rest/validation/ProductDtoValidator.java
+++
@@ -1,8 +0,0 @@
-package com.trymicroservice.mymicroservicetry.rest.validation;
-
-import com.trymicroservice.mymicroservicetry.rest.dto.AuserDTO;
-import org.springframework.stereotype.Component;
-
-@Component
-public class ProductDtoValidator implements BaseValidator<AuserDTO>{
-}
